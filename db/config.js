087ab9a2c38e4824dc97f799e816bd35b46be829
mohsin <sherazmohsin257@gmial.com>
2019-07-14 11:33:28 +0500

    db config

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/consumerdb', function(err, connection){
+mongoose.connect('mongodb+srv://mohsin:<password>@cluster0-m7tgs.mongodb.net/test?retryWrites=true&w=majority', function(err, connection){
     console.log(err||connection);
 });
 
- 
+  
