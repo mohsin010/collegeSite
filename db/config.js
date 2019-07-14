e7671580f5a0ed461c1010e70f36c4702e845238
mohsin <sherazmohsin257@gmial.com>
2019-07-14 12:23:42 +0500

    db config

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI/consumerdb || 'mongodb://127.0.0.1:27017/consumerdb'
+const url = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/consumerdb'
 
 mongoose.connect( url, function(err, connection){
     console.log(err||connection);
