4d73342472aef367c983ba4f115833ce53075954
mohsin <sherazmohsin257@gmial.com>
2019-07-14 23:07:03 +0500

    ddd

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 const url = 'mongodb://127.0.0.1:27017/consumerdb'
-const MONGODB_URI = 'mongodb+srv://mohsin:MD005090509@cluster0-m7tgs.mongodb.net/test?retryWrites=true&w=majority'
+const MONGODB_URI = 'mongodb+srv://mohsin:12345@cluster0-m7tgs.mongodb.net/collegeDb?retryWrites=true&w=majority'
 
 
 mongoose.connect( MONGODB_URI , function(err, connection){
