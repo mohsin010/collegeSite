b0065f2dc7c0186275230a67cfaa42b6748daffd
mohsin <sherazmohsin257@gmial.com>
2019-07-16 15:32:58 +0500

    aaa

--- db/config.js
+++ db/config.js
@@ -4,7 +4,7 @@ const url = 'mongodb://127.0.0.1:27017/consumerdb'
 const MONGODB_URI = 'mongodb+srv://mohsin:12345@cluster0-m7tgs.mongodb.net/collegeDb?retryWrites=true&w=majority'
 
 
-mongoose.connect( url , function(err, connection){
+mongoose.connect( MONGODB_URI , function(err, connection){
     console.log(err||connection);
 });
 
