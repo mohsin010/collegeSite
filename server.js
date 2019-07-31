bd1946961e816bc70de805b687ffb8de3270853b
mohsin <sherazmohsin257@gmial.com>
2019-07-31 21:06:45 +0500

    'tessting'

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.use('/', userRoutes);
 // })
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build' , 'uploads'))
+    app.use( 'build', express.static('client/build'))
     app.get('*', (req, res) =>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html', 'uploads'));
     });
